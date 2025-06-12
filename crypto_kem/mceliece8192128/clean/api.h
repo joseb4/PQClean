af9ab17ef3bc930bcf6ab6ad39cfb908164c0ecb
joseb4 <josebamartinez2@gmail.com>
2025-06-12 12:34:28 +0200

    solving problems

--- crypto_kem/mceliece8192128/clean/api.h
+++ crypto_kem/mceliece8192128/clean/api.h
@@ -4,15 +4,9 @@
 #include <stdint.h>
 
 #define PQCLEAN_MCELIECE8192128_CLEAN_CRYPTO_ALGNAME "Classic McEliece 8192128"
-<<<<<<< HEAD
-#define PQCLEAN_MCELIECE8192128_CLEAN_CRYPTO_PUBLICKEYBYTES 1357824
-#define PQCLEAN_MCELIECE8192128_CLEAN_CRYPTO_SECRETKEYBYTES 14120
-#define PQCLEAN_MCELIECE8192128_CLEAN_CRYPTO_CIPHERTEXTBYTES 208
-=======
 #define PQCLEAN_MCELIECE8192128_CLEAN_CRYPTO_PUBLICKEYBYTES PK_NROWS*PK_NCOLS/8
 #define PQCLEAN_MCELIECE8192128_CLEAN_CRYPTO_SECRETKEYBYTES (32+8)+SYS_T*2+((1 << (GFBITS - 4)) * (2 * GFBITS - 1)) + SYS_N / 8 //6492
 #define PQCLEAN_MCELIECE8192128_CLEAN_CRYPTO_CIPHERTEXTBYTES PK_NROWS/8 //96 size_of( H*e) = PK_NCOLS
->>>>>>> master
 #define PQCLEAN_MCELIECE8192128_CLEAN_CRYPTO_BYTES 32
 
 int PQCLEAN_MCELIECE8192128_CLEAN_crypto_kem_enc(
@@ -33,42 +27,4 @@ int PQCLEAN_MCELIECE8192128_CLEAN_crypto_kem_keypair
     uint8_t *sk
 );
 
-<<<<<<< HEAD
-=======
-int PQCLEAN_MCELIECE8192128_CLEAN_custom_encrypt
-(
-    unsigned char *y, 
-    const unsigned char *x, 
-    const unsigned char *G, 
-    const unsigned char *w
-);
-
-int PQCLEAN_MCELIECE8192128_CLEAN_syndrome
-(
-    unsigned char *s, 
-    const unsigned char *pk, 
-    const unsigned char *e
-);
-
-void PQCLEAN_MCELIECE8192128_CLEAN_codeword
-(
-    unsigned char *xG, 
-    const unsigned char *pk, 
-    const unsigned char *x
-); 
-
-int PQCLEAN_MCELIECE8192128_CLEAN_extract_preimage
-(
-    unsigned char *preimage_out, 
-    const unsigned char *c, 
-    const unsigned char *sk,
-    unsigned char *e
-);
-
-void PQCLEAN_MCELIECE8192128_CLEAN_gen_weight
-(
-    unsigned char *e, 
-    int weight
-);
->>>>>>> master
 #endif
